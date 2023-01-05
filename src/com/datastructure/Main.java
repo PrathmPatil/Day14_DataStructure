6731b54ba4106a63b0a775e87e86604f2383e0f5
PrathmPatil <prathmpatil2818@gmail.com>
2023-01-05 18:44:07 +0530

    Add at first in List

---
+++ src/com/datastructure/Main.java
@@ -0,0 +1,19 @@
+package com.datastructure;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		LinkList ll = new LinkList();
+		
+//		ll.add(56);
+//		ll.add(30);
+//		ll.add(70);
+		
+		ll.addFirst(70);
+		ll.addFirst(30);
+		ll.addFirst(56);
+		ll.display();
+	}
+
+}
