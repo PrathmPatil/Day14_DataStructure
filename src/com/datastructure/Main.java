10a0834b2a9a66938ef891d024053beb477453db
PrathmPatil <prathmpatil2818@gmail.com>
2023-01-05 18:55:14 +0530

    Add in appending way

--- src/com/datastructure/Main.java
+++ src/com/datastructure/Main.java
@@ -6,13 +6,13 @@ public class Main {
 	public static void main(String[] args) {
 		LinkList ll = new LinkList();
 		
-//		ll.add(56);
-//		ll.add(30);
-//		ll.add(70);
+		ll.add(56);
+		ll.add(30);
+		ll.add(70);
 		
-		ll.addFirst(70);
-		ll.addFirst(30);
-		ll.addFirst(56);
+//		ll.addFirst(70);
+//		ll.addFirst(30);
+//		ll.addFirst(56);
 		ll.display();
 	}
 
